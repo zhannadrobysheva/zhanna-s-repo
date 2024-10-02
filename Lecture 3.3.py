5fc8af5cb03f13980571a3c85999e9bafad4c458
zhannadrobysheva <drobysheva.zhanna06@mail.ru>
2024-10-02 18:23:02 +0300

    Add files via upload

---
+++ Lecture 3.3.py
@@ -0,0 +1,10 @@
+x=int(input())
+y=int(input())
+if x>0 and y>0:
+    print('Первая')
+if x<0 and y>0:
+    print('Вторая')
+if x<0 and y<0:
+    print('Третья')
+if x>0 and y<0:
+    print('Четвертая')
