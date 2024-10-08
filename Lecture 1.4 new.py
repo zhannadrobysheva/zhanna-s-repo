1074ca5bd19fb91a7631003090bfb339c5778f8b
zhannadrobysheva <drobysheva.zhanna06@mail.ru>
2024-10-08 18:21:14 +0300

    Add files via upload

---
+++ Lecture 1.4 new.py
@@ -0,0 +1,5 @@
+number = int(input("Введите число: "))
+next_number = number + 1
+previous_number = number - 1
+print("За числом", number, "следует число", next_number)
+print("Перед числом", number, "следует число", previous_number)
